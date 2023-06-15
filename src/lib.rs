88e501ba14315a73296c1b48989f5868b9c066c8
Finomnis <Finomnis@users.noreply.github.com>
2023-06-15 23:22:49 +0200

    Update issue tracker url

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 #![deny(missing_docs)]
 #![forbid(unsafe_code)]
 #![doc = include_str!("../README.md")]
-#![doc(issue_tracker_base_url = "https://github.com/Finomnis/st7565/issues")]
+#![doc(issue_tracker_base_url = "https://github.com/Finomnis/ws2812-flexio/issues")]
 
 /// FlexIO driver
 pub mod flexio;
